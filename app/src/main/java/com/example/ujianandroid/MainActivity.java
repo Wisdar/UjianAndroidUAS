58ab342a91108d0aa72e59266b8a22d0d7b266b7
Wisdar <wisnusmp620@gmail.com>
2024-01-10 20:47:21 +0700

    Perubahan MainActivity.java sesuai instruksi

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
